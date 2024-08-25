6e4cba5039644f30677911aa2eb87da0784a88c2
saurabh-rajput-15 <saurabhgirase2005@gmail.com>
2024-08-25 22:27:52 +0530

    1

---
+++ app/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <>
+        <div className='bg-[#46f5c1]  '>
+           szdfxcghjk </div>
+    </>
+  )
+}
+
+export default Hero
---
+++ app/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+
+const page = () => {
+  return (
+   <>
+    <Navbar />
+    <Hero />
+   </>
+  )
+}
+
+export default page
