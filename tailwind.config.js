1ddc3905c0cc20fb1b9cd857555ef9edfcff0c03
saurabh-rajput-15 <saurabhgirase2005@gmail.com>
2024-08-27 11:30:23 +0530

    3

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,7 @@ module.exports = {
           '100%': { transform: 'translateX(-100%)' }
         },
         slideRight: {
-          '0%': { transform: 'translateX(0%)' },
+          '0%': { transform: 'translateX(-50%)' },
           '100%': { transform: 'translateX(100%)' }
         }
       },
